4dc9dc9081efa3cbde4db091488da70fc8cdba52
Otabek <ganijanovich94@gmail.com>
2022-08-29 08:28:36 +0500

    feat: grid darslari

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1-masala.
+// greeting() degan funksiya yarating.
+// greeting() funksiyasi chaqirilganda console ga "Salom Dunyo!" degan yozuv chiqsin.
+// Masalan:
+// greeting() => "Salom Dunyo!"
+
+// 2-masala.
+// intro(name, age) degan funksiya yarating.
+// intro(name, age) name parametrga string qabul qiladi, age parametrga son qabul qiladi.
+// intro(name, age) chaqirilganda "Mening ismim <name>. Yoshim <age> da." degan yozuv console ga chiqsin.
+// Masalan:
+// intro("Akmal", 25) => "Mening ismim Akmal. Yoshim 25 da."
+// intro("Malika", 42) => "Mening ismim Malika. Yoshim 42 da."
+
+// 3-masala
+// const friends = ["Aziz", "Jasur", "Bexruz", "Mirjon"]; degan array berilgan.
+// addFriend(name) degan funksiya yarating.
+// addFriend(name) name parametrga string qabul qiladi.
+// addFriend(name) chaqirilgandan <name> ni friends arrayiga qo'shsin.
+// Masalan:
+// addFriend("Oybek") =>
+//                      friends = ["Aziz", "Jasur", "Bexruz", "Mirjon", "Oybek"]
+// addFriend("Zafar") =>
+//                      friends = ["Aziz", "Jasur", "Bexruz", "Mirjon", "Oybek", "Zafar"]
+// addFriend("Nodir") =>
+//                      friends = ["Aziz", "Jasur", "Bexruz", "Mirjon", "Oybek", "Zafar", "Nodir"]
+
+// 4-masala
+// addNumber(a, b) degan funksiya yarating.
+// addNumber(a, b) a va b parametrlari son bo'lsin va a bilan b ni bir biriga qo'shib qaytarsin.
+// Masalan:
+// addNumber(12, 7) => 127
+// addNumber(5, 38) => 538
+// addNumber(10, 1) => 101
